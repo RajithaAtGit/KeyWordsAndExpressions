fb114cd254a4e7700153ee46bb5556750ebc864e
Goonawardana R N W <gtrxone@gmail.com>
2021-11-15 13:20:21 +0530

    identify expressions

--- src/com/gtrxone/Main.java
+++ src/com/gtrxone/Main.java
@@ -3,6 +3,20 @@ package com.gtrxone;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        // In the following coed that I will type below, write down what part of the code are expressions.
+        int score = 100; // expression -> score = 100
+        if (score > 99) { // expression -> score > 99
+            System.out.println("You got the high score!");  // expression -> "You got thr high score!"
+            score = 0; // expression -> score = 0
+        }
+
+
     }
 }
